686e4877771bc5170db8ddb011dfa135b13a0ef1
André Lucio <43222862+andrellucio@users.noreply.github.com>
2021-04-06 15:26:08 -0300

    Add files via upload

---
+++ aula011.js
@@ -0,0 +1,11 @@
+var veloc = 75
+var veloc01 = 60
+console.log(`A velocidade é de ${veloc}km/h`)
+
+if(veloc > veloc01)
+    {console.log(`Você ultrapassou a velocidade permitida !! ${veloc01}km/h`)
+
+    }
+    else{
+console.log(`OK. Dirija com segurança !!`)
+    }
